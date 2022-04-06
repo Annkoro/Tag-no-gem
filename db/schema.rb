af021f6f6b0ea01a9d94c6a246be23c6212bc7a0
Annkoro <0vk325551w33k8n@gmail.com>
2022-04-06 12:56:59 +0000

    [Add]devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_04_123830) do
+ActiveRecord::Schema.define(version: 2022_04_04_130415) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title", null: false
@@ -34,6 +34,19 @@ ActiveRecord::Schema.define(version: 2022_04_04_123830) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["name"], name: "index_tags_on_name", unique: true
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "taggings", "posts"
